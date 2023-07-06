03394630b17ab1d5cb3a44da907120a25a98b857
lilzem <dmytrynskiy74@gmail.com>
2023-07-06 14:59:24 +0300

    updated slice

--- slice.js
+++ slice.js
@@ -1,6 +1,8 @@
 const mySlice = (arr, begin, end) => {
   if (end === undefined) {
     end = arr.length;
+  } else if (end < 0) {
+    end = arr.length + end;
   }
   let result = [];
   for (i = 0; i < arr.length; i++) {
@@ -13,5 +15,5 @@ const mySlice = (arr, begin, end) => {
 };
 
 let array = ["ant", "bison", "camel", "duck", "elephant"];
-console.log(mySlice(array, 2, 4));
-console.log(array.slice(2, 4));
+console.log(mySlice(array, 2, -1));
+console.log(array.slice(2, -1));
