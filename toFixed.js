eac271e8b13e127175a3f5389f610043afb9c174
lilzem <dmytrynskiy74@gmail.com>
2023-07-15 18:39:36 +0300

    updated toFixed and trim

--- toFixed.js
+++ toFixed.js
@@ -1,1 +1,27 @@
-console.log(isNumber(" ".charCodeAt()));
+Number.prototype.MyToFixed = function (fractionDigits) {
+  var number = String(this);
+  var digits = fractionDigits || 0,
+    length;
+
+  if (digits < 0 && digits > 20)
+    throw "RangeError: toFixed() digits argument must be between 0 and 100";
+
+  var decimal = number.match(/(?<=\.)(\d*)/g);
+  var factor = Math.pow(10, digits);
+  if (decimal && decimal[0].length >= digits)
+    return String(Math.round(Number(number + "1") * factor) / factor);
+  else {
+    var length = digits - (decimal ? decimal[0].length : 0);
+    var delimiter = number.includes(".") || !length ? "" : ".";
+    return String(number) + delimiter + "0".repeat(length);
+  }
+};
+
+function test() {
+  console.log((1.5e20).MyToFixed(2));
+  console.log((-35.35).toFixed(2));
+  console.log((-35.9).toFixed(2));
+  console.log((-35).toFixed(2));
+}
+
+test();
--- trim.js
+++ trim.js
@@ -1,14 +1,20 @@
-let str = 0;
+let str = "hello my dear     ";
 let result = "";
 
 if (typeof str !== "string") {
   throw new SyntaxError();
 } else {
-  for (char of str) {
-    if (char.charCodeAt() !== " ".charCodeAt()) {
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    let prev = str[i - 1];
+    let next = str[i + 1];
+    if (char !== " ") {
+      result += char;
+    } else if (char === " " && prev !== " " && next !== " ") {
       result += char;
     }
   }
 }
 
 console.log(result);
+console.log("hello my dear     ".trim());
